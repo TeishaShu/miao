81172a692e80a56a7d2d9dfd4001358acb0aa745
teisha <teisha@twgreengold.com>
2020-06-08 16:57:46 +0800

    訂單管理api修改完成

--- src/components/admin_product/admin_book.vue
+++ src/components/admin_product/admin_book.vue
@@ -1,49 +1,51 @@
 <template>
   <div>
-  <!--這頁的page有紅字、日期的部分-->
+    <!--這頁的page有紅字、日期的部分-->
     <!--loading-->
     <div class="vld-parent">
       <loading :active.sync="isLoading"></loading>
     </div>
-    
+
     <div class="table-responsive">
       <table>
         <thead>
           <tr>
             <th width="30"></th>
             <th width="120">購買人資訊</th>
+            <th width="120">修改訂單</th>
             <th width="120">訂購產品</th>
             <th width="120">應付金額</th>
             <th width="150">是否付款</th>
             <th width="120">購買時間</th>
-            <th width="120">修改訂單</th>
           </tr>
         </thead>
         <tbody>
           <tr
-            v-for="(item,index) in tempProduct"
+            v-for="(item, index) in tempProduct"
             :key="index"
             :class="{ notUse: !item.is_paid }"
           >
             <td>{{ item.num }}</td>
             <td>
               <ul v-for="person in item.user" :key="person.id">
-                <li>姓名:{{person.name}}</li>
-                <li>地址:{{person.address}}</li>
+                <li>姓名:{{ person.name }}</li>
+                <li>地址:{{ person.address }}</li>
               </ul>
             </td>
+            <td><i class="fas fa-pencil-alt" @click="openModel(item)"></i></td>
             <td>
-              <ul><!--訂購產品項目的顯示...-->
-                <li v-for="(pro,key) in item.products" :key="pro.id">
-                {{pro.product.title}} * {{pro.qty}}</li>
+              <ul>
+                <!--訂購產品項目的顯示...-->
+                <li v-for="(pro, key) in item.products" :key="pro.id">
+                  {{ pro.product.title }} * {{ pro.qty }}
+                </li>
               </ul>
             </td>
             <td style="text-align:right">{{ item.total | currency }}</td>
             <td :class="{ okGreen: item.is_paid }">
               {{ item.is_paid ? "已付款" : "未付款" }}
             </td>
-            <td>{{dateForm(item.create_at)}}</td>
-            <td><i class="fas fa-pencil-alt" @click="openModel(item)"></i></td>
+            <td>{{ dateForm(item.create_at) }}</td>
           </tr>
         </tbody>
       </table>
@@ -81,6 +83,7 @@
               class="close"
               data-dismiss="modal"
               aria-label="Close"
+              @click="cancelEdit"
             >
               <span aria-hidden="true" style="color:#fff">&times;</span>
             </button>
@@ -100,17 +103,26 @@
                         </tr>
                       </thead>
                       <tbody class="pro">
-                        <tr v-for="item in editItem.products" :key="item.id">
-                          <td>{{item.product.title}}</td>
-                          <td>${{item.product.price}}</td>
+                        <tr v-for="(item, key) in newEdit.products" :key="item.id">
+                          <td>{{ item.product.title }}</td>
+                          <td>${{ item.product.price }}</td>
                           <td>
-                            <input type="text" v-model.trim="item.qty" />
+                            <input
+                              autocomplete="on"
+                              pattern="\d*"
+                              type="number"
+                              v-model.trim="item.qty"
+                              min="1"
+                              @change="updateTotal(key, item.qty, item.product.price)"
+                            />
                           </td>
-                          <td>${{item.price}}</td>
+                          <td>${{ item.total }}</td>
                         </tr>
                         <tr>
                           <td colspan="3">總金額</td>
-                          <td>$ 還需要改.改數量(不能負的).金額也要算過在送出</td>
+                          <td>
+                            $ {{newEdit.total}}
+                          </td>
                         </tr>
                       </tbody>
                     </table>
@@ -124,32 +136,35 @@
                           <th colspan="2">購買人資訊</th>
                         </tr>
                       </thead>
-                      <tbody v-for="person in editItem.user" :key="person.id">
+                      <tbody v-for="person in newEdit.user" :key="person.id">
                         <tr>
                           <td>姓名</td>
-                          <td>{{person.name}}</td>
+                          <td>{{ person.name }}</td>
                         </tr>
                         <tr>
                           <td>電話</td>
-                          <td>{{person.tel}}</td>
+                          <td>{{ person.tel }}</td>
                         </tr>
                         <tr>
                           <td>E-mail</td>
-                          <td>{{person.email}}</td>
+                          <td>{{ person.email }}</td>
                         </tr>
                         <tr>
                           <td>地址</td>
-                          <td><input type="text" v-model.trim="person.address"/></td>
+                          <td>
+                            <input type="text" v-model.trim="person.address" />
+                          </td>
                         </tr>
                         <tr>
                           <td>留言</td>
-                          <td><textarea row="4" v-model="person.name"></textarea></td>
+                          <td>
+                            <textarea row="4" v-model="person.name"></textarea>
+                          </td>
                         </tr>
                       </tbody>
                     </table>
                   </div>
                 </div>
-
               </div>
             </div>
           </div>
@@ -158,6 +173,7 @@
               type="button"
               class="btn btn-outline-secondary"
               data-dismiss="modal"
+              @click="cancelEdit"
             >
               取消
             </button>
@@ -179,43 +195,45 @@
 @import "../../assets/sass/variables.scss";
 @import "../../assets/sass/page.scss";
 @import "admin_product.scss";
-th{
+th {
   white-space: nowrap;
   padding: 0 5px;
 }
-td{
-  text-align:center;
+td {
+  text-align: center;
 }
-i{
-  cursor:pointer;
+i {
+  cursor: pointer;
 }
-.model{
-  td{
-    text-align:left;
+.model {
+  td {
+    text-align: left;
   }
-  th{
-    text-align:left;
+  th {
+    text-align: left;
   }
-  input,,textarea{
+  input,
+  ,
+  textarea {
     border: 1px solid #ced4da;
     border-radius: 0.25rem;
-    margin:5px 0;
-    width:100%;
+    margin: 5px 0;
+    width: 100%;
   }
 }
-.modelUser{
-  margin-top:30px;
+.modelUser {
+  margin-top: 30px;
 }
-.pro{
-  input{
-    width:50px;
-    text-align:center !important;
+.pro {
+  input {
+    width: 50px;
+    text-align: center !important;
   }
 }
 </style>
 <script>
-import Paginate from "vuejs-paginate";
 import $ from "jquery";
+import Paginate from "vuejs-paginate";
 export default {
   data() {
     return {
@@ -224,31 +242,56 @@ export default {
         // current_page: 1 //這個刪除
       },
       isLoading: false,
-      editItem :{
-        create_at:1585625169,
-        id:"",
-        is_paid:false,
-        num:1,
-        products:{},
-        total:1,
-        user:{},
+      editItem: {
+        create_at: 1585625169,
+        id: "",
+        is_paid: false,
+        num: 1,
+        products: {},
+        total: 1,
+        user: {},
       },
-      status:{
-        fileUpLoading:false
-      }
+      newEdit: {
+        create_at: 1585625169,
+        id: "",
+        is_paid: false,
+        num: 1,
+        products: {},
+        total: 1,
+        user: {},
+      },
+      status: {
+        fileUpLoading: false,
+      },
     };
   },
   components: {
     Paginate,
   },
-  watch:{
-    
-  },
   methods: {
+    updateTotal(key, qty, price) { //wow!!
+     
+      // valid  "".e.<1
+      // if (valid) {
+
+      // }
+      const newEdit_products = this.newEdit.products;
+      newEdit_products[key].total = qty * price;
+      newEdit_products[key].final_total = qty * price;
+      let newTotal = 0;
+      for (let prop in newEdit_products){
+        newTotal += newEdit_products[prop].total
+      }
+      this.newEdit.total = newTotal;
+    },
+    cancelEdit(){
+      this.newEdit = {};
+      this.editItem = {};
+    },
     getBook() {
       this.isLoading = true;
       const api = `${process.env.VUE_APP_DEFAULT_SRC}/api/teisha/admin/orders?page=${this.dataPage.current_page}`;
-      this.$http.get(api).then(response => {
+      this.$http.get(api).then((response) => {
         this.isLoading = false;
         // console.log('getBook',response.data);
         if (response.data.success) {
@@ -257,14 +300,14 @@ export default {
         }
       });
     },
-    dateForm(num){
+    dateForm(num) {
       let dd = new Date(num);
-      return `${dd.getFullYear()}/${dd.getMonth()+1}/${dd.getDate()}`;
+      return `${dd.getFullYear()}/${dd.getMonth() + 1}/${dd.getDate()}`;
     },
-    edit(item){
+    edit(item) {
       this.editItem = item;
       const api = `${process.env.VUE_APP_DEFAULT_SRC}/api/teisha/admin/order/${item.id}`;
-      this.$http.put(api,{data}).then(response => {
+      this.$http.put(api, { data }).then((response) => {
         this.isLoading = false;
         // console.log(response.data);
         if (response.data.success) {
@@ -273,32 +316,34 @@ export default {
         }
       });
     },
-    openModel(item){
-      if(item.is_paid){
-        return
+    openModel(item) {
+      if (item.is_paid) {
+        return;
       }
       this.editItem = {};
-      this.editItem = Object.assign({},item);
+      this.newEdit = {};
+      this.editItem = item; // 保留原本的
+      this.newEdit =  JSON.parse(JSON.stringify(item));
       $("#editModal").modal("show");
     },
-    updateProduct(){
+    updateProduct() {
       this.isLoading = true;
-      const editt = this.editItem;
-      // editt.total = 
+      const editt = this.newEdit;
+      // editt.total =
       const api = `${process.env.VUE_APP_DEFAULT_SRC}/api/teisha/admin/order/${editt.id}`;
-      this.$http.put(api,{"data":editt}).then(response => {
+      this.$http.put(api, { data: editt }).then((response) => {
         this.isLoading = false;
         // console.log('updateProduct',response.data);
         if (response.data.success) {
           this.getBook();
           $("#editModal").modal("hide");
-          alert(response.data.message)
+          alert(response.data.message);
         }
       });
     },
   },
   created() {
     this.getBook();
-  }
+  },
 };
 </script>
