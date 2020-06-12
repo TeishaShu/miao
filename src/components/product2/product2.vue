74997226b8e37785887630a56e23f7ce0a138649
teisha <teisha@twgreengold.com>
2020-06-12 09:38:03 +0800

    product send 紅字

--- src/components/product2/product2.vue
+++ src/components/product2/product2.vue
@@ -1,4 +1,5 @@
-<template><!--加入購物車後不知道為何會出現紅字-->
+<template
+  ><!--加入購物車後不知道為何會出現紅字-->
   <div class="container out">
     <!--loading-->
     <div class="vld-parent">
@@ -9,15 +10,18 @@
         <img :src="dataProdtct.imageUrl" alt="" class="img-fluid" />
       </div>
       <div class="col-md-7 inner">
-        <h3>{{dataProdtct.title}}</h3>
-        <p class="type">{{dataProdtct.category}}</p>
+        <h3>{{ dataProdtct.title }}</h3>
+        <p class="type">{{ dataProdtct.category }}</p>
         <ul class="detail">
-          <li v-for="el in detail(dataProdtct.description)">{{el}}</li>
+          <li v-for="el in detail(dataProdtct.description)">{{ el }}</li>
         </ul>
-        <p class="price"><em>NT{{dataProdtct.origin_price | currency}}</em><br />NT{{dataProdtct.price | currency}}</p>
+        <p class="price">
+          <em>NT{{ dataProdtct.origin_price | currency }}</em
+          ><br />NT{{ dataProdtct.price | currency }}
+        </p>
         <div class="add">
           <i class="fas fa-minus" @click="changeNum(-1)"></i>
-          <input type="text" v-model.trim="selectNum"/>
+          <input type="text" v-model.trim="selectNum" />
           <i class="fas fa-plus" @click="changeNum(1)"></i>
         </div>
         <a href="#" class="addCart" @click.prevent="addCart">加入購物車</a>
@@ -34,60 +38,72 @@
 import cartBtn from "../../layout/footerStyle/cartBtn.vue";
 export default {
   components: {
-    cartBtn
+    cartBtn,
   },
-  data(){
-    return{
-      dataProdtct:{},
-      isLoading:false,
-      addCartSend:{},
-      selectNum:1,
-    }
+  data() {
+    return {
+      dataProdtct: {
+        category: "",
+        content: "",
+        description: "",
+        id: "",
+        image: "",
+        imageUrl: "",
+        is_enabled: 0,
+        num: 0,
+        origin_price: "",
+        price: "",
+        title: "",
+        unit: "",
+      },
+      isLoading: false,
+      addCartSend: {},
+      selectNum: 1,
+    };
   },
   methods: {
     getProduct() {
       this.isLoading = true;
       const id = this.$route.params.id;
       const api = `${process.env.VUE_APP_DEFAULT_SRC}/api/teisha/product/${id}`;
-      console.log('1')
-      this.$http.get(api).then(response => {
-        console.log('2')
+      this.$http.get(api).then((response) => {
         this.isLoading = false;
         if (response.data.success) {
           this.dataProdtct = response.data.product;
-          console.log('3')
         }
       });
     },
-    addCart(){
+    addCart() {
+      const vm = this;
       this.isLoading = true;
       this.addCartSend.product_id = this.dataProdtct.id;
       this.addCartSend.qty = this.selectNum;
       const api = `${process.env.VUE_APP_DEFAULT_SRC}/api/teisha/cart`;
-      this.$http.post(api,{data:this.addCartSend}).then(response => {
+      this.$http.post(api, { data: this.addCartSend }).then((response) => {
         this.isLoading = false;
         // console.log('11',response.data)
         if (response.data.success) {
-          this.dataProdtct = response.data.product;
-          this.getProduct() //加入購物車後不知道為何會出現紅字.所以加這段.還是沒消失但是可以跑
+          this.dataProdtct = response.data.data.product;
         }
       });
     },
-    changeNum(num){
+    changeNum(num) {
       let orNum = Number(this.selectNum);
-      if(num<0 && orNum <= 1){
+      if (num < 0 && orNum <= 1) {
         this.selectNum = 1;
-      }else{
+      } else {
         this.selectNum = orNum + num;
       }
     },
-    detail(txt){
-      if(txt === undefined){return}; // 注意傳入的值.不要使function壞掉
+    detail(txt) {
+      if (txt === undefined) {
+        return;
+      } // 注意傳入的值.不要使function壞掉
       return txt.split("@");
-    }
+    },
   },
   created() {
     this.getProduct();
-  }
+  },
 };
 </script>
